33170ed48fb793d348f55f68d1213f5d20c9834a
santiago mesa <thiagomesa21@gmail.com>
2025-03-04 20:14:04 -0500

    Refactor

--- src/screens/ProductDetail.jsx
+++ src/screens/ProductDetail.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import ProductItem from '../components/ProductItem';
+import products from '../data/products';
 
-export default function ProductDetail({ route }) {
-  const { product } = route.params
-
+export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 22 }}>{product.name}</Text>
-      <Text style={{ fontSize: 18, marginVertical: 10 }}>{product.description}</Text>
-      <Text style={{ fontSize: 16, color: 'gray' }}>${product.price}</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ProductItem product={item} onPress={() => navigation.navigate('ProductDetail', { product: item })} />
+        )}
+      />
     </View>
-  )
+  );
 }
