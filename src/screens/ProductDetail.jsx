c68b9b7b08f6268e125a4e41c276c0bb46f8a0ff
santiago mesa <thiagomesa21@gmail.com>
2025-03-04 19:32:06 -0500

    agregamos segunda pantalla llamada ProductDetail

---
+++ src/screens/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ProductDetail({ route }) {
+  const { product } = route.params
+
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      <Text style={{ fontSize: 22 }}>{product.name}</Text>
+      <Text style={{ fontSize: 18, marginVertical: 10 }}>{product.description}</Text>
+      <Text style={{ fontSize: 16, color: 'gray' }}>${product.price}</Text>
+    </View>
+  )
+}
