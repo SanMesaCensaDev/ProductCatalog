165680abe82a5033da8465f01d4e524ab6d2fad3
SanMesaCensaDev <sanmesaserna@gmail.com>
2025-03-04 19:56:34 -0500

    Update products.js

---
+++ src/data/products.js
@@ -0,0 +1,5 @@
+export default[
+    { id: 1, name: 'Producoto 1', description: 'descripcion del producto 1', price: 50000},
+    { id: 2, name: 'Producoto 2', description: 'descripcion del producto 1', price: 60000},
+    { id: 3, name: 'Producoto 3', description: 'descripcion del producto 1', price: 20000},
+]
